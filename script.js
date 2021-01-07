b8a369d2fcff3c0d0ffdf9eb08ee712943f274f5
Nathan Chan <nathan.tai.on.chan@gmail.com>
2021-01-07 00:45:41 +0000

    removed 1 million chiky nuggets

--- script.js
+++ script.js
@@ -16,189 +16,172 @@ let asgore;
 let winHeight = 750;
 let winWidth = 1150;
 let score = 0;
-let circleX = winWidth/2;
+let circleX = winWidth  /  2;
 let circleY = 125;
 
 //functions
 function colourR() {
-	noteColour = redNote
-	lineColour = "red"
+  noteColour = redNote
+  lineColour = "red"
 }
 function colourM() {
-	noteColour = magentaNote;
-	lineColour = "magenta"
+  noteColour = magentaNote;
+  lineColour = "magenta"
 }
 function colourO() {
-	noteColour = orangeNote;
-	lineColour = "orange"
+  noteColour = orangeNote;
+  lineColour = "orange"
 }
 function colourB() {
-	noteColour = blueNote;
-	lineColour = "blue"
+  noteColour = blueNote;
+  lineColour = "blue"
 }
 function colourY() {
-	noteColour = yellowNote;
-	lineColour = "yellow"
+  noteColour = yellowNote;
+  lineColour = "yellow"
 }
-function blueNoteConsole() {
-	console.log("BlueNoteLoaded")
-}
-function redNoteConsole() {
-	console.log("RedNoteLoaded")
-}
-function magentaNoteConsole() {
-	console.log("MagentaNoteLoaded")
-}
-function frenzyNoteConsole() {
-	console.log("frenzyNoteLoaded")
-}
-function orangeNoteConsole() {
-	console.log("orangeNoteLoaded")
-}
-function yellowNoteConsole() {
-	console.log("yellowNoteLoaded")
-}
-function pressedNoteConsole() {
-	console.log("PressedNoteLoaded")
+function colourLB() {
+	noteColour = lightBlueNote;
+	lineColour = "lightBlue"
 }
 
 function lineTopScale() {
-	img.resize(200,200)
-} 
+  img.resize(200,  200)
+}
 
 function scaleBackground() {
-  gameBackground.resize(winWidth, winHeight);
+	gameBackground.resize(winWidth, winHeight);
 }
 function preload() {
-	soundFormats('mp3')
-	redNote = loadImage("Notes/RedNote.png", redNoteConsole);
-	blueNote = loadImage("Notes/BlueNote.png", blueNoteConsole);
-	magentaNote = loadImage("Notes/MagentaNote.png", magentaNoteConsole);
-	frenzyNote = loadImage("Notes/FrenzyNote.png", frenzyNoteConsole);
-	orangeNote = loadImage("Notes/OrangeNote.png", orangeNoteConsole);
-	yellowNote = loadImage("Notes/YellowNote.png", yellowNoteConsole);
-	pressedNote = loadImage("Notes/PressedNote.png", pressedNoteConsole);
-	frame = loadImage("Images/Frame.png")
-  gameBackground = loadImage("Images/gameBackgroundPOG.jpg", scaleBackground);
+  soundFormats('mp3')
+  redNote = loadImage("Notes/RedNote.png");
+  blueNote = loadImage("Notes/BlueNote.png", );
+  magentaNote = loadImage("Notes/MagentaNote.png");
+  frenzyNote = loadImage("Notes/FrenzyNote.png");
+  orangeNote = loadImage("Notes/OrangeNote.png");
+  yellowNote = loadImage("Notes/YellowNote.png");
+	lightBlueNote = loadImage("Notes/lightBlueNote.png")
+  pressedNote = loadImage("Notes/PressedNote.png");
+  frame = loadImage("Images/Frame.png")
+  asgoreSong = loadImage("SongImages/asgorePic.png", asgoreSong)
+	gameBackground = loadImage("Images/gameBackgroundPOG.jpg", scaleBackground);
 }
 
 function setup() {
-	//framesPerSecond(fps);
-		asgore = loadSound("Songs/asgore.mp3", playAsgore);
-  doggo = loadImage("Images/doggo.jpg")
-	createCanvas(winWidth, winHeight);
-	circleX = windowWidth / 2
-	colorMode(HSB);
-	imageMode(CENTER);
-	buttonRed = createButton('Red');
-	buttonRed.position(900,30);
-
-	buttonMagenta = createButton('Magenta');
-	buttonMagenta.position(900,50);
-
-	buttonOrange = createButton('Orange');
-	buttonOrange.position(900,70);
-
-	buttonBlue = createButton('Blue');
-	buttonBlue.position(900,90);
-
-  buttonYellow = createButton('Yellow')
-  buttonYellow.position(900,110);
-  lineColour = "blue"
-  noteColour = blueNote;
-
-	noteSpeedSlider = createSlider(1, 25, 5)
-	noteSpeedSlider.position(12, 12)
+  //framesPerSecond(fps);
+	doggo = loadImage("Images/doggo.jpg")
+  createCanvas(winWidth, winHeight);
+  circleX = windowWidth / 2
+  colorMode(HSB);
+  imageMode(CENTER);
+  buttonRed = createButton('Red');
+  buttonRed.position(900,  30);
+  buttonMagenta = createButton('Magenta');
+  buttonMagenta.position(900,  50);
+  buttonOrange = createButton('Orange');
+  buttonOrange.position(900,  70);
+  buttonBlue = createButton('Blue');
+  buttonBlue.position(900,  90);
+	buttonYellow = createButton('Yellow')
+	buttonYellow.position(900,  110);
+	buttonLBlue = createButton('Light Blue')
+	buttonLBlue.position(900, 130);
+	lineColour = "blue"
+	noteColour = blueNote;
+  noteSpeedSlider = createSlider(1, 25, 5)
+  noteSpeedSlider.position(12, 12)
+  //button test
+	asgoreButton = createButton("Play Asgore");
 }
 
 function draw() {
-  //BOX
-  //package length object package getMatchedCSSRules SVGPathSegCurvetoQuadraticSmoothRel webkitConvertPointFromNodeToPage frenzyNoteConsole getMatchedCSSRules HTMLAllCollection encodeURIComponent webkitConvertPointFromNodeToPage SVGPathSegCurvetoQuadraticSmoothRel VideoPlaybackQuality break PressedNoteLoaded webkitConvertPointFromNodeToPage pageYOffset
-  
-  //image(pogbox, (circleX*1.5), 125, 130, 50);
-	//buttons for customizing note colours
-  imageMode(CORNER);
-	background(gameBackground);
-  imageMode(CENTER);
-	buttonRed.mousePressed(colourR);
-	buttonMagenta.mousePressed(colourM);
-	buttonOrange.mousePressed(colourO);
-	buttonBlue.mousePressed(colourB);
-	buttonYellow.mousePressed(colourY);
-	// Runs in a loop
-	
-  fill("cyan")
-  rect(0, 0, winWidth, 125);
-	//asgore = loadSound("Songs/asgore.mp3", playAsgore); 
-	//player note pressing things
-	fill(255, 204, 100);
-	//notes (not really)
-	
-	for (var i = 0; i < 4; i++) {
-		stroke(lineColour)
-		line(circleX + (40 * i), 125, circleX + (40 * i), winHeight)
-		image(noteColour, circleX + (40 * i), circleY, 30)
-	}
+  //buttons for customizing note colours
+	imageMode(CORNER);
+  background(gameBackground);
+	imageMode(CENTER);
+  buttonRed.mousePressed(colourR);
+  buttonMagenta.mousePressed(colourM);
+  buttonOrange.mousePressed(colourO);
+  buttonBlue.mousePressed(colourB);
+  buttonYellow.mousePressed(colourY);
+	buttonLBlue.mousePressed(colourLB);
+  asgoreButton.mousePressed(buttonAsgore)
+  // Runs in a loop
+
+	fill("cyan")
+	rect(0, 0, winWidth, 125);
+  //player note pressing things
+  fill(255, 204, 100);
+  //notes (not really)
+
+  for (var i = 0; i < 4; i++) {
+    stroke(lineColour)
+    line(circleX + (40 * i), 125, circleX + (40 * i), winHeight)
+    image(noteColour, circleX + (40 * i), circleY, 30)
+  }
 
 
 
-	//player note control press pog score generator epicness idk 
-	for (var l = 0; l < 4; l++) {
+  //player note control press pog score generator epicness idk 
+  for (var l = 0; l < 4; l++) {
+		image(frame, circleX, winHeight - 80, 40, 40);
+    image(frame, circleX + 40, winHeight - 80, 40, 40);
+    image(frame, circleX + (40 * 2), winHeight - 80, 40, 40);
+    image(frame, circleX + (40 * 3), winHeight - 80, 40, 40);
+    image(noteColour, circleX, winHeight - 80, 35, 35);
+    image(noteColour, circleX + 40, winHeight - 80, 35, 35);
+    image(noteColour, circleX + (40 * 2), winHeight - 80, 35, 35);
+    image(noteColour, circleX + (40 * 3), winHeight - 80, 35, 35);
+    if (aPressed) {
+      image(pressedNote, circleX, winHeight - 80, 32, 32);
+    }
+    if (sPressed) {
+      image(pressedNote, circleX + 40, winHeight - 80, 32, 32);
+    }
+    if (dPressed) {
+      image(pressedNote, circleX + (40 * 2), winHeight - 80, 32, 32);
+    }
+    if (fPressed) {
+      image(pressedNote, circleX + (40 * 3), winHeight - 80, 32, 32);
+    }
+  }
+  circleY += noteSpeedSlider.value()
+  //Teleports notes back to the top of the screen
+  if (circleY >= winHeight - 20) {
+    circleY = 125;
+  }
 
-		image(frame, circleX, winHeight - 80, 35, 35);
-
-		image(frame, circleX + 40, winHeight - 80, 35, 35);
-
-		image(frame, circleX + (40 * 2), winHeight - 80, 35, 35);
-
-		image(frame, circleX + (40 * 3), winHeight - 80, 35, 35);
-
-		if (aPressed) {
-			image(pressedNote, circleX, winHeight - 80, 32, 32);
-		}
-		if (sPressed) {
-			image(pressedNote, circleX + 40, winHeight - 80, 32, 32);
-		}
-		if (dPressed) {
-			image(pressedNote, circleX + (40 * 2), winHeight - 80, 32, 32);
-		}
-		if (fPressed) {
-			image(pressedNote, circleX + (40 * 3), winHeight - 80, 32, 32);
-		}
-
-	}
-	circleY += noteSpeedSlider.value()
-	//Teleports notes back to the top of the screen
-	if (circleY >= winHeight - 20) {
-		circleY = 125;
-	}
-
-	//player controls alpha
-	if (keyIsDown(left)) {
-		aPressed = true;
-	} else {
-		aPressed = false;
-	}
-	if (keyIsDown(leftmiddle)) {
-		sPressed = true;
-	} else {
-		sPressed = false;
-	}
-	if (keyIsDown(rightmiddle)) {
-		dPressed = true;
-	} else {
-		dPressed = false;
-	}
-	if (keyIsDown(right)) {
-		fPressed = true;
-	} else {
-		fPressed = false;
-	}
-
+  //player controls alpha
+  if (keyIsDown(left)) {
+    aPressed = true;
+  } else {
+    aPressed = false;
+  }
+  if (keyIsDown(leftmiddle)) {
+    sPressed = true;
+  } else {
+    sPressed = false;
+  }
+  if (keyIsDown(rightmiddle)) {
+    dPressed = true;
+  } else {
+    dPressed = false;
+  }
+  if (keyIsDown(right)) {
+    fPressed = true;
+  } else {
+    fPressed = false;
+  }
+}
+function buttonAsgore() {
+  console.log("loading asgore")
+  asgore = loadSound("Songs/asgore.mp3", playAsgore);
 }
 
 function playAsgore() {
-  console.log(asgoreLoaded)
-  asgore.volume(1)
-  asgore.autoplay(true);
+  console.log("asgorePlaying")
+  asgore.play();
+}
+function asgoreSong() {
+	console.log("asgorePicLoaded")
 }
